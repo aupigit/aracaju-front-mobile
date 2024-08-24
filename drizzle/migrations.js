aee96f16bbda91947eb93d1af8698b7750be08c4
gabriel.rohden <gabriel_rohden@outlook.com>
2024-08-24 13:09:34 -0300

    chore: re-gen db

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_illegal_imperial_guard.sql';
+import m0000 from './0000_romantic_captain_britain.sql';
 
   export default {
     journal,
